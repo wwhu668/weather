0d5bf649c010be0e7f5158a4fed24f0afc5de4e3
Autumn <wwhu668@163.com>
2018-10-09 07:31:05 +0000

    Apply fixes from StyleCI

--- src/Exceptions/HttpException.php
+++ src/Exceptions/HttpException.php
@@ -1,10 +1,16 @@
 <?php
 
+/*
+ * This file is part of the overtrue/weather.
+ *
+ * (c) wwhu668<wwhu668@163.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
 
 namespace Wwhu\Weather\Exceptions;
 
-
 class HttpException extends Exception
 {
-
 }
